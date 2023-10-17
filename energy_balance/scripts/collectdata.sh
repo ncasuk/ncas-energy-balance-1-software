e4a85dacd380be1a7b87dbc83b4c369637a7b24a
James Groves <james.groves@ncas.ac.uk>
2023-10-17 15:04:27 +0000

    small shell script that activates the venv and then runs the daily collect data. This is useful for point a cron at

---
+++ energy_balance/scripts/collectdata.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+PATH=/home/ammss-user/ncas-energy-balance-1-software/energy_balance/scripts
+source /home/ammss-user/ncas-energy-balance-1-software/venv/bin/activate 
+python /home/ammss-user/ncas-energy-balance-1-software/energy_balance/scripts/download_data.py
